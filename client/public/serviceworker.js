faceba22425c31a1a9e53edda3155642610f23a2
andretwx <andretai.wx@gmail.com>
2020-09-01 22:50:09 +0800

    Still trying to fix service worker...

--- client/public/serviceworker.js
+++ client/public/serviceworker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'v1';
-const urlsToCache = ['./index.html', './offline.html'];
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html', 'offline.html'];
 
 const self = this;
 
